ba90a6caddd0593a0c3cff14a77d77199ced6b95
Ashiqur2279 <ashiqurrahman2279@gmail.com>
2023-04-15 22:59:45 +0600

    all saved

---
+++ abc.js
@@ -0,0 +1,56 @@
+// function compare(a, b) {
+
+//     if (a.toString() === b) {
+   
+//     return true;
+   
+//     } else {
+   
+//     return false;
+   
+//     }
+   
+//    }
+   
+//    const result = compare(25, 25);
+   
+//    console.log(result);
+
+// function compare(a, b) {
+
+//     if (a == b) {
+   
+//     return true;
+   
+//     } else {
+   
+//     return false;
+   
+//     }
+   
+//    }
+   
+//    const result = compare(15, "15");
+   
+//    console.log(result);
+
+// let a = "hi";
+
+// let b = "there"
+
+// console.log(a+b)
+
+let count = 0;console.log(parseInt("count"+ 1));
+
+console.log(Math.pow(4, 0.5)+"spiderman"+"987"+Math.sqrt(9))
+
+function generatePin(){ return Math.floor(Math.random()*90000) + 10000; }
+
+console.log(generatePin())
+
+const product = 5; 
+const price = "7"; 
+const subTotal = product * price; 
+const tax = subTotal/10; 
+const total = subTotal + tax; 
+console.log(total)
---
+++ assignment 4.js
@@ -0,0 +1,99 @@
+// ---------------------Problem One------------------------
+
+// Whatever number you input Here, at first your input will multiply by 3. then adding 10. after that it will be divided by 2 and minus 5. It will be your final result .
+
+
+// function mindGame(number) {
+//   const multiply = number * 3;
+//   const total = multiply + 10;
+//   const division = total / 2;
+//   finalResult = division - 5;
+// }
+// inputValue = 33;
+// mindGame(inputValue);
+// console.log(finalResult);
+
+
+
+
+// ----------------------problem two-------------------------
+
+// Whatever string value you can input here. but the result will be your string character length is it even or odd.
+
+
+// function evenOdd(string){
+//     if(string.length % 2 == 0){
+//         console.log('even')
+//     }
+//     else{
+//         console.log('odd')
+//     }
+// }
+// let inputValue = "phero";
+// evenOdd(inputValue)
+
+
+
+
+// ----------------------problem three-------------------------
+
+// if your input value is smaller than 7, then it will show the difference. But if Not, then it will show the double of your input.
+
+
+// function isLGSeven(number){
+//     let sum = number-7;
+//     if(sum < 7){
+//         console.log(sum)
+//     }
+//     else{
+//         console.log(number*2)
+//     }
+// }
+// let inputValue = 15 ;
+// isLGSeven(inputValue)
+
+
+
+
+// ----------------------problem four-------------------------
+// if your input array element will smaller than zero, then it will make a new array. named bad Data. and the final result will be the length of bad Data.
+
+
+
+// badData =[];
+// goodData=[];
+// function findingBadData(numbers) {
+//     for (let i = 0; i < numbers.length; i++) {
+//         if (numbers[i] < 0) {
+//             badData.push(numbers[i])
+//         }
+//         else{
+//             goodData.push(numbers[i])
+//         }
+//     }
+//     console.log (badData.length);
+// }
+// const inputValue = [ -4, -9, -5, -33, -55 ];
+// findingBadData(inputValue)
+
+
+
+// ----------------------problem five-------------------------
+// here you can input three types number. each number has own power. if the total diamond power will greater than double of 1000 then the final result will be total diamond minus 2000. but if not, the result will the total of diamond of three input.
+
+
+// function gemsToDiamond(firstFriend, secondFriend, thirdFriend){
+//     const firstFriendDiamond = firstFriend * 21;
+//     const secondFriendDiamond = secondFriend * 32;
+//     const thirdFriendDiamond = thirdFriend * 43;
+//     const totalDiamond = firstFriendDiamond + secondFriendDiamond + thirdFriendDiamond;
+//     if(totalDiamond>1000*2){
+//         console.log(totalDiamond-2000)
+//     }
+//     else{console.log(totalDiamond)}
+// }
+// let firstFriendGems = 100;
+// let secondFriendGems = 5;
+// let thirdFriendGems = 1;
+// gemsToDiamond(firstFriendGems, secondFriendGems, thirdFriendGems)
+ 
